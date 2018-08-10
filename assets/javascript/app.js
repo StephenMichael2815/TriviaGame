108c62c9cda105b4d95c78bfd2f9d2ca418fe0c1
Stephen Michael <stephenmichael@Stephens-MacBook-Pro.local>
2018-08-09 23:57:21 -0600

    Added code to try and make it work

---
+++ assets/javascript/app.js
@@ -0,0 +1,36 @@
+var counter = 0;
+    score = 0
+
+
+
+
+
+var questions = [{
+    question: "Albert Einstein was awarded the Nobel Prize in Physics?",
+    choices: ["True, False"],
+    correctAnswer: 0
+}, {
+    question: "The first name of Kramer in Seinfeld is Cosmo?",
+    choices: ["True, False"],
+    correctAnswer: 0
+}, {
+    question: "Paprika” is the Hungarian word for “pepper?",
+    choices: ["True, False"],
+    correctAnswer: 0
+}, {
+    question: "Snow White’s seven dwarfs all worked as lumberjacks?",
+    choices: ["True, False"],
+    correctAnswer: 1
+}, {
+    question: "The favorite food of the Teenage Mutant Ninja Turtles is pizza?",
+    choices: ["True, False"],
+    correctAnswer: 0
+}];
+
+var quizOver = false;
+
+// have to add if else statements
+// add a reset feature
+// add a score feature
+// get HTML onto the page
+//add buttons
